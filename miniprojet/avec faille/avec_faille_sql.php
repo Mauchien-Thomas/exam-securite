d32366da55bc90d4656b86404b561868a404d583
Mauchien-Thomas <71167353+Mauchien-Thomas@users.noreply.github.com>
2021-11-07 20:23:29 +0100

    Update avec_faille_sql.php

--- miniprojet/avec faille/avec_faille_sql.php
+++ miniprojet/avec faille/avec_faille_sql.php
@@ -1,26 +1,5 @@
 <?php
 require_once('bddconnetion.php');
-// $resultat = $pdo ->query("SELECT * FROM Utilisateurs");
-// var_dump($resultat);
-
-// $pdo->query("CREATE TABLE IF NOT EXISTS Utilisateurs ( 
-//   id            VARCHAR( 250 )         PRIMARY KEY,
-//   mdp           VARCHAR( 250 )
-// );");
-
-// $stmt = $pdo->prepare("INSERT INTO Utilisateurs (id, mdp) VALUES (:id, :mdp)");
-// $result = $stmt->execute(array(
-//     'id'         => "admin",
-//     'mdp'       =>  "thomas"
-// ));
-
-// identifiant = admin, mot de passe = thomas
-// 'OR 1 = 1 /*
-
-// $stmt = $pdo->prepare("SELECT * FROM Utilisateurs WHERE id = :id");
-// $stmt->execute(array('id' => 'admin'));
-// $result = $stmt->fetchAll();
-// print_r($result);
 
 echo '<div class="menu">'
 .'<ul>'
